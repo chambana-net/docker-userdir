954ed4f6710e59dee023e30866157efb6ba9e7ce
Josh King <jking@chambana.net>
2020-03-29 16:34:58 -0400

    Fix userfile path, other cleanup.

--- bin/authcommand.sh
+++ bin/authcommand.sh
@@ -1,28 +1,28 @@
-#!/bin/bash - 
+#!/bin/bash -
 #===============================================================================
 #
 #          FILE: authcommand.sh
-# 
+#
 #         USAGE: ./authcommand.sh <username>
-# 
+#
 #   DESCRIPTION: User creation script to be run by OpenSSH AuthorizedKeysCommand
-# 
+#
 #       OPTIONS: ---
 #  REQUIREMENTS: ---
 #          BUGS: ---
 #         NOTES: ---
-#        AUTHOR: Josh King (jking@chambana.net) 
-#  ORGANIZATION: 
+#        AUTHOR: Josh King (jking@chambana.net)
+#  ORGANIZATION:
 #       CREATED: 05/10/2016 10:41
 #      REVISION:  ---
 #===============================================================================
 
-set -o nounset                              # Treat unset variables as an error
+set -o nounset # Treat unset variables as an error
 
 #Check to make sure argument is provided
 #[[ -v "1" ]] || exit 1
 
-USERFILE=/etc/ssh/auth/chambana.yml
+USERFILE=/etc/ssh/auth/users.yml
 USER="$1"
 
 # Improved parse_yaml from https://github.com/jasperes/bash-yaml, credit to Jonathan Peres
@@ -35,17 +35,15 @@ parse_yaml() {
 
     s='[[:space:]]*'
     w='[a-zA-Z0-9_.-]*'
-    fs="$(echo @|tr @ '\034')"
+    fs="$(echo @ | tr @ '\034')"
 
     (
         sed -e '/- [^\“]'"[^\']"'.*: /s|\([ ]*\)- \([[:space:]]*\)|\1-\'$'\n''  \1\2|g' |
-
-        sed -ne '/^--/s|--||g; s|\"|\\\"|g; s/[[:space:]]*$//g;' \
-            -e "/#.*[\"\']/!s| #.*||g; /^#/s|#.*||g;" \
-            -e "s|^\($s\)\($w\)$s:$s\"\(.*\)\"$s\$|\1$fs\2$fs\3|p" \
-            -e "s|^\($s\)\($w\)${s}[:-]$s\(.*\)$s\$|\1$fs\2$fs\3|p" |
-
-        awk -F"$fs" '{
+            sed -ne '/^--/s|--||g; s|\"|\\\"|g; s/[[:space:]]*$//g;' \
+                -e "/#.*[\"\']/!s| #.*||g; /^#/s|#.*||g;" \
+                -e "s|^\($s\)\($w\)$s:$s\"\(.*\)\"$s\$|\1$fs\2$fs\3|p" \
+                -e "s|^\($s\)\($w\)${s}[:-]$s\(.*\)$s\$|\1$fs\2$fs\3|p" |
+            awk -F"$fs" '{
             indent = length($1)/2;
             if (length($2) == 0) { conj[indent]="+";} else {conj[indent]="";}
             vname[indent] = $2;
@@ -55,10 +53,8 @@ parse_yaml() {
                     printf("%s%s%s%s=(\"%s\")\n", "'"$prefix"'",vn, $2, conj[indent-1],$3);
                 }
             }' |
-
-        sed -e 's/_=/+=/g' |
-
-        awk 'BEGIN {
+            sed -e 's/_=/+=/g' |
+            awk 'BEGIN {
                 FS="=";
                 OFS="="
             }
@@ -66,7 +62,7 @@ parse_yaml() {
                 gsub("-|\\.", "_", $1)
             }
             { print }'
-    ) < "$yaml_file"
+    ) <"$yaml_file"
 }
 
 create_variables() {
